bfec5ce03bca890d18fd954454bca82725fbceef
Tuxfan <jonas@familie-wuepping.de>
2025-12-18 21:19:14 +0100

    Refactor configuration-properties

--- src/main/java/com/example/linklistapi/LinkListApiApplication.java
+++ src/main/java/com/example/linklistapi/LinkListApiApplication.java
@@ -2,8 +2,10 @@ package com.example.linklistapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LinkListApiProperties.class)
 public class LinkListApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/linklistapi/LinkListApiProperties.java
+++ src/main/java/com/example/linklistapi/LinkListApiProperties.java
@@ -1,31 +1,11 @@
-package com.example.linklistapi.configuration;
+package com.example.linklistapi;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "link-list-api")
 public class LinkListApiProperties {
-    private String apiUser;
-    private String password;
     private String meilisearchUrl;
     private String meilisearchApiKey;
-
-    public String getApiUser() {
-        return apiUser;
-    }
-
-    public void setApiUser(String apiUser) {
-        this.apiUser = apiUser;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getMeilisearchUrl() {
         return meilisearchUrl;
